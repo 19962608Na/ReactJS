c2ff8fb2293f4345ba73ad32000a53f0871bedef
Nguyen Hong Hang Phuong <xna.giotnang@gmail.com>
2021-12-09 15:54:00 +0700

    update 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>lab01 updated
+        <p>lab01 updated 2
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
