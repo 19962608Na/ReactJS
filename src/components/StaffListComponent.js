69cd2e5f9d3ed143bc2f7343be3b3cff48a211db
Nguyen Hong Hang Phuong <xna.giotnang@gmail.com>
2021-12-21 14:20:20 +0700

    tieu chi co ban

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,28 +1,27 @@
 import React, { Component } from "react";
-import { STAFFS } from "../shared/staffs";
 import {Card, CardText, CardBody, CardTitle} from 'reactstrap';
+import dateFormat from 'dateformat'; 
 
 class StaffList extends Component {
     constructor(props) {
         super(props);
         this.state = { 
             selectedName: null
-         }
-    }
-
+         };
+    };
     onNameSelect(staff) {
         this.setState({selectedName: staff});
     }
-
+   
     renderStaff(staff) {
         if(staff != null)
             return(
                 <Card>
                     <CardBody>
                         <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                        <CardText>Ngày sinh: {staff.dOB}</CardText>
-                        <CardText>Ngày vào công ty: {staff.startDate}</CardText>
-                        <CardText>Phòng ban: {staff.department}</CardText>
+                        <CardText>Ngày sinh: {dateFormat(staff.doB, 'dd/mm/yyyy')}</CardText>
+                        <CardText>Ngày vào công ty: {dateFormat(staff.startDate, 'dd/mm/yyyy')}</CardText>
+                        <CardText>Phòng ban: {staff.department.name}</CardText>
                         <CardText>Số ngày nghỉ ăn lương: {staff.annualLeave}</CardText>
                         <CardText>Số ngày làm thêm: {staff.overTime}</CardText>
                     </CardBody>
